81ccebc5564c4591c663ed438225b6ddf19dcfca
ant4g0nist <ant4g0nist@users.noreply.github.com>
2022-11-22 16:56:43 +0000

    feat: adds github actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,9 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'wefuzz', // Usually your GitHub org/user name.
+  organizationName: 'wefuzz.github.io', // Usually your GitHub org/user name.
   projectName: 'crypto-books', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
